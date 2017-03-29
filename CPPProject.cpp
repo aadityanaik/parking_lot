82f603953b150acac5df4fb4c9fa63c54991455a
Utkarsh Pant <utkarsh.pant@outlook.com>
2017-03-29 22:07:50 +0530

    Test commit to branch 'branch_utkarsh'
    This is a test commit, to see how things are working. Made a few minute changes, mainly cosmetic.

--- CPPProject.cpp
+++ CPPProject.cpp
@@ -1,4 +1,4 @@
-      #include <iostream>
+#include <iostream>
 #include <cstdlib>
 #include <windows.h>
 #include <conio.h>
@@ -21,19 +21,24 @@ class parking_lot
     bool space[10][10];
     int count[4];
     float time_allot;
+
 public:
-    parking_lot()
+    parking_lot()                                       //constructor to initialise values of each spot in the lot;
     {
-        for(int i = 0; i < 10; i++){
-            for(int j = 0; j < 10; j++){
+        for(int i = 0; i < 10; i++)
+        {
+            for(int j = 0; j < 10; j++)
+            {
                 space[i][j] = 0;
             }
         }
 
-        for(int i = 0; i < 4; i++){
+        for(int i = 0; i < 4; i++)
+        {
             count[i] = 0;
         }
     }
+
     friend void getdata(parking_lot*, vehicle*);
     bool allot_space();
     void disp_lot();
@@ -44,52 +49,74 @@ class vehicle
     string first_name, surname;
     int time_in, time_out;
     float time_parked;
+
 public:
-    friend void getdata(parking_lot*, vehicle*);
-    float charge(int);
-    bool chk_name(string , string);
+    friend void getdata (parking_lot*, vehicle*);
+    float charge (int);
+    bool chk_name (string , string);
 };
 
 //Definition of functions
 
-bool vehicle::chk_name(string a, string b)                                      //checks if the name exists. Will change when we learn file handling
+bool vehicle::chk_name (string a, string b)                                      //checks if the name exists. Will change when we learn file handling
 {
-    if(a == first_name && b == surname){
+    if(a == first_name && b == surname)
+    {
         return 1;
     }
-    else{
+    else
+    {
         return 0;
     }
 }
 bool parking_lot::allot_space()                                                 //returns 1 if space allocated, 0 otherwise
 {
     int row, column;
-    if(time_allot >= 4){                                                        //more than 4 hours
-        if(count[0] >= 20){
+    if(time_allot >= 4)
+    {                                                                           //more than 4 hours
+        if(count[0] >= 20)
+        {
             cout << "No more space for you" << endl;                            //in case the entire section is occupied
             return 0;
         }
-        else{
-            do{
+
+        else
+        {
+            do
+            {
                 column = rand() % 10;                                           //randomly generate a spot--- column
                 row = rand() % 2 + 65;                                          //row
-            }while(space[row - 65][column] == YES);                             //keeps looping until the slot generated is empty
+            }
+
+            while (space[row - 65][column] == YES);                             //keeps looping until the slot generated is empty
+
             cout << "Your space is " << char(row) << column + 1 << endl;
             space[row - 65][column] = 1;
             count[0]++;
             return 1;
         }
     }
-    if(time_allot < 4 && time_allot >=3){                                       //more than 3 hours
-        if(count[1] >= 20){
+
+
+    if(time_allot < 4 && time_allot >=3)
+        {                                                                       //more than 3 hours
+
+        if(count[1] >= 20)
+        {
             cout << "No more space for you" << endl;                            //in case the entire section is occupied
             return 0;
         }
-        else{
-            do{
+
+        else
+        {
+            do
+            {
                 column = rand() % 10;                                           //randomly generate a spot--- column
                 row = rand() % 2 + 67;                                          //row
-            }while(space[row - 65][column] == YES);                             //keeps looping until the slot generated is empty
+            }
+
+            while (space[row - 65][column] == YES);                             //keeps looping until the slot generated is empty
+
             cout << "Your space is " << char(row) << column + 1 << endl;
             space[row - 65][column] = 1;
             count[1]++;
@@ -97,38 +124,55 @@ bool parking_lot::allot_space()                                                 
         }
     }
 
-    if(time_allot < 3 && time_allot >= 1.5){
+    if (time_allot < 3 && time_allot >= 1.5)
+        {
         if(count[2] >= 36){
             cout << "No more space for you" << endl;                            //in case the entire section is occupied
             return 0;
         }
-        else{
-            do{
+
+        else
+        {
+            do
+            {
                 row = rand() % 6 + 69;                                          //randomly generate row
-                if(row > 70){
+
+                if (row > 70)
+                {
                     column = rand() % 4;                                        //random generation of column for rows J, I, H, G
+
                     if(column >= 2)
                         column = column + 6;
                 }
                 else
                     column = rand() % 10;                                       //random generation of column for rows E, F
-            }while(space[row - 65][column] == YES);
+            }
+            while(space[row - 65][column] == YES);
+
             cout << "Your space is " << char(row) << column + 1 << endl;
             space[row - 65][column] = 1;
             count[2]++;
             return 1;
         }
     }
-    if(time_allot < 1.5 && time_allot > 0){
-        if(count[3] >= 24){
+
+    if (time_allot < 1.5 && time_allot > 0)
+    {
+        if(count[3] >= 24)
+        {
             cout << "No more space for you" << endl;                            //in case the entire section is occupied
             return 0;
         }
-        else{
-            do{
+
+        else
+        {
+            do
+            {
                 row = rand() % 4 + 71;                                          //random generation of row
                 column = rand() % 6 + 2;                                        //random generation of column
-            }while(space[row - 65][column] == YES);
+            }
+            while (space[row - 65][column] == YES);
+
             cout << "Your space is " << char(row) << column + 1 << endl;
             space[row - 65][column] = 1;
             count[3]++;
@@ -141,18 +185,22 @@ void getdata(parking_lot *x, vehicle *y)                                    //ta
 {
     cout << "Enter the amount of time you want to park for (in hours):\t";
     cin >> (x -> time_allot);
-    if(cin.fail()){                                                         //cin function fails in the extraction process
+    if (cin.fail())
+    {                                                                       //cin function fails in the extraction process
         cout << "Bad Parameters. Try again:" << endl;
         cin.clear();                                                        //clears out the buffer and resets the cin function
         cin.ignore(32767, '\n');                                            //removes/ignores the bad input
     }
-    if((x -> allot_space())){                                               //if space is alloted
+
+    if((x -> allot_space()))
+    {                                                                       //if space is alloted
         (y -> time_in) = time(NULL);                                        //time of entry
         cout << "Enter your name as <first name> <surname> :\t";
         cin >> y -> first_name >> y -> surname;                             //takes in user name
     }
 
-    else{
+    else
+    {
         cout << "Please try after some time" << endl;                       //we need more space, or funds, or a new job
     }
 }
@@ -168,21 +216,28 @@ void parking_lot::disp_lot()
 {
     int m ,n;
     cout << "   ";
-    for(n = 0; n < 10; n ++){                                               //Printing column headings appropriately spaced
+    for (n = 0; n < 10; n ++)
+    {                                                                       //Printing column headings appropriately spaced
         cout << n + 1 << "  ";
     }
+
     cout << endl;
-    for(m = 0; m < 10; m++){
+
+    for (m = 0; m < 10; m++)
+    {
         cout << char(m + 65) << "  ";                                       //Printing row names
-        for(n = 0; n < 10; n++){
+        for (n = 0; n < 10; n++)
+        {
             cout << space[m][n] << "  ";                                    //Printing the value under each spot
         }
         cout << endl;
+
         if(m % 2 == 1)
             cout << endl;
     }
+
     cout << "   ";
-    for(n = 0; n < 4; n++)
+    for (n = 0; n < 4; n++)
         cout << "   ";
     cout << "Entry" << endl;                                                //Printing the entry point of the lot
 }
@@ -191,13 +246,15 @@ int main()
 	char vehicle_choice;
     char choice;                                                            //Choices, because we want a complicated program worth at least 5 marks
     char *currenttime;                                                      //pointer to a string
-    /*parking_lot *slot;                                                      //pointer to an object
+
+    /*parking_lot *slot;                                                    //pointer to an object
     parking_lot lot;
     slot = &lot;                                                            //slot points to lot*/
-    parking_lot *slot4 = new parking_lot;
-    parking_lot *slot2 = new parking_lot;
+
+    parking_lot *FWslot = new parking_lot;
+    parking_lot *TWslot = new parking_lot;
     int i = 0;
-    vehicle car[1000], bike[1000];                                                      //array of 1000 cars, because we are too popular for our own good
+    vehicle car[1000], bike[1000];                                          //array of 1000 cars, because we are too popular for our own good
     struct tm *t;                                                           //pointer to struct tm
     time_t y;                                                               // variable of a custom datatype
     string fname, sname;
@@ -215,31 +272,35 @@ int main()
         switch(choice)                                                      //Switch... case, because if conditions are too mainstream
         {
         case '1':
-        	
-        	cout<<"Enter Your Vehicle Choice:"<<endl<<"1)4-Wheeler"<<endl<<"2)2-Wheeler"<<endl;
+
+        	cout<<"Enter Your Vehicle Choice: "<<endl<<"1>  4-Wheeler"<<endl<<"2>   2-Wheeler"<<endl;
         	cin>>vehicle_choice;
         	switch(vehicle_choice)
         	{
         		case '1':
-        			cout << "\t" << "\t" << "\t" << currenttime << endl;                //shows the time the function is called
+        			cout << "\t" << "\t" << "\t" << currenttime << endl;    //shows the time the function is called
         			slot4 -> disp_lot();
-        			getdata(slot4, (car + i));
+        			getdata(FWslot, (car + i));
                     break;
+
                 case '2':
-                	cout << "\t" << "\t" << "\t" << currenttime << endl;                //shows the time the function is called
+                	cout << "\t" << "\t" << "\t" << currenttime << endl;    //shows the time the function is called
         			slot2 -> disp_lot();
-					getdata(slot2,(bike+i));     
+					getdata(TWslot, (bike+i));
 					break;
 			}
             break;
-            
+
         case '2':
             cout << "Enter your name as <first name> <surname> :\t";
             cin >> fname >> sname;
-            for(int i = 0; i < 1000; i++){
-                if(car[i].chk_name(fname, sname)){                          //Checks if a car in the parking spot is associated with entered name
-                    cout << "Thank you for using PARKINATOR parking systems. Your charge is INR " << car[i].charge()<<endl;
-                                                                            //Sorry, couldn't think of a better name
+
+            for (int i = 0; i < 1000; i++)
+            {
+                if (car[i].chk_name(fname, sname))                                      //Checks if a car in the parking spot is associated with entered name
+                {
+                    cout << "Thank you for using PARKINATOR parking systems. Your charge is INR " << car[i].charge()<<endl; //Sorry, couldn't think of a better name
+
                 }
             }
             break;
@@ -249,7 +310,10 @@ int main()
             goto start;
             break;
         }
+
         i++;
-    }while(1);
+    }
+
+    while(1);
     return 0;
 }
